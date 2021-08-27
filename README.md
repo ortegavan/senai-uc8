b3d2186d89d9d3484f56194d8cb50c8f502e1ca2
Van Ortega <ortegavan@live.com>
2021-08-27 16:40:15 -0300

    códigos formatados com prettier

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## SENAI - SP
 
 Repositório de exercícios da unidade curricular 8 do curso de full stack do SENAI - SP
+
+Códigos formatados com [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
--- contagem.js
+++ contagem.js
@@ -1,48 +1,48 @@
 // for
 let numeroDeAlunos = 10;
-for (let contador = 0; contador <= numeroDeAlunos; contador++){
-    if (contador === 0){
-        console.log("O número atual é ZERO");
-    } else if (contador % 2 == 0) {
-        console.log("O número " + contador + " é PAR");
-    } else {
-        console.log("O número " + contador + " é ÍMPAR");
-    }
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+	if (contador === 0) {
+		console.log("O número atual é ZERO");
+	} else if (contador % 2 == 0) {
+		console.log("O número " + contador + " é PAR");
+	} else {
+		console.log("O número " + contador + " é ÍMPAR");
+	}
 }
 
 // while
 let contador = 0;
-while (contador < numeroDeAlunos){
-    if (contador === 0){
-        console.log("O número atual é ZERO");
-    } else if (contador % 2 == 0) {
-        console.log("O número " + contador + " é PAR");
-    } else {
-        console.log("O número " + contador + " é ÍMPAR");
-    }
-    contador++;
+while (contador < numeroDeAlunos) {
+	if (contador === 0) {
+		console.log("O número atual é ZERO");
+	} else if (contador % 2 == 0) {
+		console.log("O número " + contador + " é PAR");
+	} else {
+		console.log("O número " + contador + " é ÍMPAR");
+	}
+	contador++;
 }
 
 // do..while
 contador = 0;
 do {
-    if (contador === 0){
-        console.log("O número atual é ZERO");
-    } else if (contador % 2 == 0) {
-        console.log("O número " + contador + " é PAR");
-    } else {
-        console.log("O número " + contador + " é ÍMPAR");
-    }
-    contador++;
+	if (contador === 0) {
+		console.log("O número atual é ZERO");
+	} else if (contador % 2 == 0) {
+		console.log("O número " + contador + " é PAR");
+	} else {
+		console.log("O número " + contador + " é ÍMPAR");
+	}
+	contador++;
 } while (contador < numeroDeAlunos);
 
 // foreach
 let listaDeAlunos = ["João", "José", "Maria", "Pedro"];
-listaDeAlunos.forEach(aluno => {
-    console.log(aluno)
+listaDeAlunos.forEach((aluno) => {
+	console.log(aluno);
 });
 
-// for..of 
-for (let aluno of listaDeAlunos){
-    console.log(aluno);
+// for..of
+for (let aluno of listaDeAlunos) {
+	console.log(aluno);
 }
